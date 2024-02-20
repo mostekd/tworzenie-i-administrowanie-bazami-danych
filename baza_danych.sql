d5541a96ed08ef0762516a330ca7a71cf4a24e02
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-20 12:43:40 +0100

    Create baza_danych.sql

---
+++ baza_danych.sql
@@ -0,0 +1,15 @@
+drop table przesylki;
+
+create table przesylki(
+    idPrzesylka int,
+    nazwa varchar(45),
+    cena double,
+    platnosci boolean,
+    idReklamacja int,
+    idKategoria int,
+    idStan int,
+    idPracownik int,
+    idOdbiorca int,
+    idNadawca int,
+    primary key (idPrzesylka,idReklamacja,idKategoria, idStan,idPracownik,idOdbiorca,idNadawca)
+    );
