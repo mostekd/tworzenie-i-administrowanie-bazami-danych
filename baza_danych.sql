ebe0165294496a742f00ed4c3d60bdef8ac84fa3
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-20 14:14:17 +0100

    Update baza_danych.sql

--- baza_danych.sql
+++ baza_danych.sql
@@ -1,4 +1,38 @@
-create table przesylki(
+create table Pracownicy(
+    idPracownik int,
+    imie varchar(45),
+	nazwisko varchar(45),
+    pesel int,
+    ulica varchar(45),
+    miasto varchar(45),
+    kod varchar(45),
+    telefon int,
+    haslo varchar(45),
+    szef boolean,
+    primary key (idPracownik)
+);
+    
+create table Odbiorca(
+    idOdbiorca int,
+    imie varchar(45),
+	nazwisko varchar(45),
+    ulica varchar(45),
+    miasto varchar(45),
+    kod varchar(45),
+    telefon int,
+    primary key (idOdbiorca)
+);
+    
+create table Reklamacje(
+    idReklamacja int,
+    Nazwa varchar(45),
+	Opis varchar(45),
+    Rozpatrzenie boolean,
+	Odpowiedz varchar(45),
+    primary key (idReklamacja)	
+);
+    
+create table Przesylki(
     idPrzesylka int,
     nazwa varchar(45),
     cena double,
@@ -10,4 +44,27 @@ create table przesylki(
     idOdbiorca int,
     idNadawca int,
     primary key (idPrzesylka,idReklamacja,idKategoria, idStan,idPracownik,idOdbiorca,idNadawca)
-    );
+);
+    
+create table Kategorie(
+    idKategoria int,
+    nazwa varchar(45),
+    primary key (idKategoria)
+);
+    
+create table Stan(
+	idStan int,
+    nazwa varchar(45),
+    primary key (idStan)
+);
+
+create table Nadawca(
+    idNadawca int,
+    imie varchar(45),
+	nazwisko varchar(45),
+    ulica varchar(45),
+    miasto varchar(45),
+    kod varchar(45),
+    telefon int,
+    primary key (idNadawca)
+);
