fbdfbb9070e67917dbbb6db20d2bc8192a8645cc
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-29 18:30:18 +0100

    Update Zadanie 3.sql

--- Zadanie 3.sql
+++ Zadanie 3.sql
@@ -1,27 +1,19 @@
 CREATE DATABASE Mostowski;
-CREATE TABLE Persons ( PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255));
  
-CREATE USER 'Mostowski_Dawid'@'localhost' IDENTIFIED BY 'qwerty';
-GRANT ALL PRIVILEGES ON Mostowski TO 'Mostowski_Dawid'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER Mostowski_Dawid WITH PASSWORD 'qwerty';
+GRANT ALL PRIVILEGES ON DATABASE Mostowski TO 'Mostowski_Dawid';
  
-SHOW GRANTS FOR 'Mostowski_Dawid'@'localhost';
+SHOW GRANTS FOR 'Mostowski_Dawid';
  
-CREATE USER 'jankowski12'@'localhost' IDENTIFIED BY 'qwerty';
-GRANT CREATE  ON Mostowski.Persons TO 'jankowski12'@'localhost';
-GRANT DELETE ON Mostowski.Persons TO 'jankowski12'@'localhost';
-GRANT DROP ON Mostowski.Persons TO 'jankowski12'@'localhost';
+CREATE USER 'jankowski12'@'localhost' WITH PASSWORD 'qwerty';
+GRANT CREATE  ON Mostowski.Persons TO 'jankowski12';
+GRANT DELETE ON Mostowski.Persons TO 'jankowski12';
+GRANT DROP ON Mostowski.Persons TO 'jankowski12';
  
-FLUSH PRIVILEGES;
- 
-SHOW GRANTS FOR 'jankowski12'@'localhost';
+SHOW GRANTS FOR 'jankowski12';
  
 SHOW GRANTS;
  
 SHOW DATABASES;
  
-DROP USER 'Mostowski_Dawid'@'localhost';
- 
-X - Nazwisko
-Y - IMIE
-C - NUMER Z DZIENNIKA
+DROP USER 'Mostowski_Dawid';
