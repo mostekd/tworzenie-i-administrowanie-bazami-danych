72c95d778263624a364426c6e6605c9abd746873
mostekd <dawid-mostowski@o2.pl>
2024-11-27 08:40:38 +0100

    update

---
+++ klasa 4/wers/Zadanie 1/firma_wers.sql
@@ -0,0 +1,31 @@
+drop database if exists firma_wers;
+create database firma_wers;
+use firma_wers;
+
+create table klienci (
+    
+);
+
+create table stanowiska (
+
+);
+
+create table pracownicy (
+
+);
+
+create table produkty (
+
+);
+
+create table typy_platnosci (
+
+);
+
+create table platnosci (
+
+);
+
+create table zamowienia (
+
+);
