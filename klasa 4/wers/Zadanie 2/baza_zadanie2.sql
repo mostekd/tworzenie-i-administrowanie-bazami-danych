dd0b00e1fb02520b238ca9f9e9d2d09821fc73ce
mostekd <dawid-mostowski@o2.pl>
2024-11-27 13:39:45 +0100

    update

--- klasa 4/wers/Zadanie 2/baza_zadanie2.sql
+++ klasa 4/wers/Zadanie 2/baza_zadanie2.sql
@@ -1,4 +1,3 @@
--- Tworzenie bazy danych
 CREATE DATABASE IF NOT EXISTS PrzykladowaBaza;
 USE PrzykladowaBaza;
 
@@ -6,7 +5,7 @@ USE PrzykladowaBaza;
 CREATE TABLE Klienci (
     klient_id INT AUTO_INCREMENT PRIMARY KEY,
     imie VARCHAR(100) NOT NULL,
-    nazwisko VARCHAR(10) NOT NULL,
+    nazwisko VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
     telefon VARCHAR(20) DEFAULT NULL
 );
@@ -15,7 +14,7 @@ CREATE TABLE Klienci (
 CREATE TABLE Produkty (
     produkt_id INT AUTO_INCREMENT PRIMARY KEY,
     nazwa VARCHAR(100) NOT NULL,
-    cena DECIMAL(10, 200) NOT NULL,
+    cena DECIMAL(10,2) NOT NULL,
     ilosc_w_magazynie INT NOT NULL CHECK (ilosc_w_magazynie >= 0)
 );
 
@@ -29,7 +28,7 @@ CREATE TABLE Zamowienia (
 
 -- Tabela SzczegolyZamowienia
 CREATE TABLE SzczegolyZamowienia (
-    zamowienie_id INT NOT NULL,
+    zamowienie_id INT NOT NULL AUTO_INCREMENT,
     produkt_id INT NOT NULL,
     ilosc INT NOT NULL CHECK (ilosc > 0),
     cena DECIMAL(10, 2) NOT NULL,
@@ -48,20 +47,20 @@ INSERT INTO Klienci (imie, nazwisko, email, telefon) VALUES
 -- Dodanie  produktów
 INSERT INTO Produkty (nazwa, cena, ilosc_w_magazynie) VALUES
 ('Laptop', 3000.00, 10),
-('Telefon', 1500.00, -5), 
-('Tablet', -800.00, 5),  
+('Telefon', 1500.00, 5), 
+('Tablet', 800.00, 5),  
 ('Monitor', 700.00, 0);  
 
 -- Dodanie  zamówień
 INSERT INTO Zamowienia (klient_id, data_zamowienia) VALUES
 (1, '2023-08-15 10:00:00'),
 (2, '2023-08-16 12:30:00'),
-(999, '2023-08-17 14:00:00'); 
+(3, '2023-08-17 14:00:00'); 
 
 -- Dodanie  szczegółów zamówień
 INSERT INTO SzczegolyZamowienia (zamowienie_id, produkt_id, ilosc, cena) VALUES
 (1, 1, 2, 3000.00),
 (1, 2, 1, 1500.00),
-(2, 2, 0, 1500.00), 
+(2, 2, 1, 1500.00), 
 (3, 3, 1, 800.00), 
-(1, 999, 1, 1000.00);  
+(1, 3, 1, 800.00);  
