dd11514b78d3b73748be78c87d14d885169462ec
Dawid Mostowski <dawid.mostowski06@proton.me>
2024-12-07 14:36:56 +0100

    update

---
+++ klasa 4/wers/WesoleMiasteczko (1).sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 05, 2024 at 10:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `WesoleMiasteczko`
+--
+CREATE DATABASE IF NOT EXISTS `WesoleMiasteczko` DEFAULT CHARACTER SET ;
+USE `WesoleMiasteczko`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Atrakcje`
+--
+-- Error reading structure for table WesoleMiasteczko.Atrakcje: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `WesoleMiasteczko`.`Atrakcje`
+-- Error reading data for table WesoleMiasteczko.Atrakcje: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `WesoleMiasteczko`.`Atrakcje`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Bilety`
+--
+-- Error reading structure for table WesoleMiasteczko.Bilety: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `WesoleMiasteczko`.`Bilety`
+-- Error reading data for table WesoleMiasteczko.Bilety: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `WesoleMiasteczko`.`Bilety`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `FAQ`
+--
+-- Error reading structure for table WesoleMiasteczko.FAQ: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `WesoleMiasteczko`.`FAQ`
+-- Error reading data for table WesoleMiasteczko.FAQ: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `WesoleMiasteczko`.`FAQ`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Oceny`
+--
+-- Error reading structure for table WesoleMiasteczko.Oceny: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `WesoleMiasteczko`.`Oceny`
+-- Error reading data for table WesoleMiasteczko.Oceny: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `WesoleMiasteczko`.`Oceny`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Uzytkownicy`
+--
+-- Error reading structure for table WesoleMiasteczko.Uzytkownicy: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `WesoleMiasteczko`.`Uzytkownicy`
+-- Error reading data for table WesoleMiasteczko.Uzytkownicy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `WesoleMiasteczko`.`Uzytkownicy`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
